f8733a5e56a0cb9219be619bf1d3825289c4dffc
AnshuSharma1 <sharmanshu96@gmail.com>
2017-12-18 21:31:44 +0530

    services section added

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -10,7 +10,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Bungee|Pacifico|Nunito|Exo|Cookie|Dosis" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Bungee|Pacifico|Nunito|Exo|Cookie|Dosis|Titillium+Web" rel="stylesheet">
 
 
         <!--Styles -->
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -37,15 +37,15 @@
 	    </div>
 	</div>
 </div>
-<div class="first text-center" style="background-color: white; padding: 40px;">
+<div class="first text-center" style=" padding: 60px 50px 0 50px;">
 	<div class="row">
-		<div class="col-md-6">
-			<img src="{{ asset('images/pot2.jpeg') }}" alt="New York" width="80%">
+		<div class="col-md-6" style="padding: 30px 0 50px 50px;">
+			<img src="{{ asset('images/pot2.jpeg') }}" alt="New York" width="80%" style="border-radius: 5px;">
 		</div>
-		<div class="col-md-6 text-left" style="padding:0 40px; color: black;">
+		<div class="col-md-6 text-left" style="padding:20px 40px 0 40px; color: #555; font-family: 'Nunito';">
 			<h2>Lorem Ipsum Dolor Amet</h2>
-	    	<hr style="border-color: black; border-width: 3px; border-radius: 3px; width: 8%; margin: 35px 0;">
-	    	<div style="padding-right: 60px; font-size: 15px; letter-spacing: 1px;">
+	    	<hr style="border-color: #555; border-width: 3px; border-radius: 3px; width: 8%; margin: 35px 0;">
+	    	<div style="padding-right: 60px; font-size: 15px; letter-spacing: 0px; line-height: 27px; ">
 	    		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 	    		<br><br>
 	    		Voluptate velit esse cillum dolore eu fugiat nulla pariatur nulla pariatur.
@@ -56,9 +56,76 @@
 		</div>
 	</div>
 </div>
-<div class="third text-center" style="padding: 30px; background-color: #ffeaea; color: black;">
-	<h3>
-		Lorem ipsum dolor sit amet consectetur.
-	</h3>
+<div class="second text-center" style="padding: 30px;color: black; background-color: #ededed;">
+	<h3 style="margin: 30px auto 60px auto; font-weight: bold; letter-spacing: 2px; font-size: 30px; font-family: 'Exo';">Our Services</h3>
+	<div class="row " style="margin-top: 50px;">
+		<div class="col-md-6 row" style="padding-left: 120px;">
+			<img src="{{ asset('images/consult.png') }}"  class="col-md-6" alt="New York" style="border-radius: 5px; width: 30%; margin-right: 20px;">
+			<span style="font-family: 'Nunito';" class="text-left col-md-6">
+				<h3 style="margin-top: 5px;">Lorem Ipsum Dolor</h3>
+				<div style="padding-right: 10%; padding-top: 2%;">
+					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+				</div>
+			</span>
+		</div>
+		<div class="col-md-6 row" style="padding-left: 80px;">
+			<img src="{{ asset('images/icon.png') }}"  class="col-md-6" alt="New York" style="border-radius: 5px; width: 28%; margin-right: 20px;">
+			<span style="font-family: 'Nunito';" class="text-left col-md-6">
+				<h3 style="margin-top: 5px;">Lorem Ipsum Dolor</h3>
+				<div style="padding-right: 15%; padding-top: 2%;">
+					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+				</div>
+			</span>
+		</div>
+	</div>
+	<div class="row " style="margin-top: 100px; margin-bottom: 50px;">
+		<div class="col-md-6 row" style="padding-left: 120px;">
+			<img src="{{ asset('images/globe.png') }}"  class="col-md-6" alt="New York" style="border-radius: 5px; width: 27%; margin-right: 35px; margin-top: 10px;" >
+			<span style="font-family: 'Nunito';" class="text-left col-md-6">
+				<h3 style="margin-top: 5px;">Lorem Ipsum Dolor</h3>
+				<div style="padding-right: 10%; padding-top: 2%;">
+					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+				</div>
+			</span>
+		</div>
+		<div class="col-md-6 row" style="padding-left: 80px;">
+			<img src="{{ asset('images/bulb.png') }}"  class="col-md-6" alt="New York" style="border-radius: 5px; width: 28%; margin-right: 20px;">
+			<span style="font-family: 'Nunito';" class="text-left col-md-6">
+				<h3 style="margin-top: 5px;">Lorem Ipsum Dolor</h3>
+				<div style="padding-right: 15%; padding-top: 2%;">
+					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+				</div>
+			</span>
+		</div>
+	</div>
 </div>
+<div class="third" style="background-color: #67CAC5; color: white; padding: 130px 100px; font-family: 'Nunito';">
+	<div class="row text-center" style="opacity: 0.9;">
+		<div class="col-md-3">
+			<i class="fa fa-globe" aria-hidden="true" style="font-size: 60px;"></i>
+			<div style="margin-top: 20px; font-size: 17px; font-family: calibri;">
+				Lorem Ipsum dolor <br> sit amet sed do
+			</div>
+		</div>
+		<div class="col-md-3">
+			<i class="fa fa-cogs" aria-hidden="true" style="font-size: 60px;"></i>
+			<div style="margin-top: 20px; font-size: 17px; font-family: calibri;">
+				Consectetur adipiscing elit <br> sed do eiusmod tempor
+			</div>
+		</div>
+		<div class="col-md-3">
+			<i class="fa fa-users" aria-hidden="true" style="font-size: 60px;"></i>
+			<div style="margin-top: 20px; font-size: 17px; font-family: calibri;">
+				Lorem Ipsum dolor <br> sit amet sed do
+			</div>
+		</div>
+		<div class="col-md-3">
+			<i class="fa fa-pie-chart" aria-hidden="true" style="font-size: 60px;"></i>
+			<div style="margin-top: 20px; font-size: 17px; font-family: calibri;">
+				Consectetur adipiscing elit <br> sed do eiusmod tempor
+			</div>
+		</div>
+	</div>
+</div>
+
 @endsection
